71d8f0370487c18898b1ef74082e5532437b1f53
fenvalle <49774262+fenvalle@users.noreply.github.com>
2020-05-24 23:01:21 -0300

    Update MusicControlsNotificationKiller.java

--- src/android/MusicControlsNotificationKiller.java
+++ src/android/MusicControlsNotificationKiller.java
@@ -5,12 +5,16 @@ import android.os.IBinder;
 import android.os.Binder;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.PowerManager;
+
+import static android.os.PowerManager.PARTIAL_WAKE_LOCK;
 
 public class MusicControlsNotificationKiller extends Service {
 
 	private static int NOTIFICATION_ID;
 	private NotificationManager mNM;
 	private final IBinder mBinder = new KillBinder(this);
+	private PowerManager.WakeLock wakeLock;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -22,16 +26,33 @@ public class MusicControlsNotificationKiller extends Service {
 		return Service.START_STICKY;
 	}
 
+
 	@Override
 	public void onCreate() {
 		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		mNM.cancel(NOTIFICATION_ID);
+		keepAwake();
 	}
 
 	@Override
 	public void onDestroy() {
 		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		mNM.cancel(NOTIFICATION_ID);
+		sleepWell();
+	}
+
+	private void keepAwake()
+	{
+		PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+		wakeLock = pm.newWakeLock(PARTIAL_WAKE_LOCK, "backgroundmode:wakelock");
+		wakeLock.acquire();
+	}
+	private void sleepWell()
+	{
+		if (wakeLock != null) {
+			wakeLock.release();
+			wakeLock = null;
+		}
 	}
 
 }
